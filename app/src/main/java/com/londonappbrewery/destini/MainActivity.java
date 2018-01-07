50e0c32a307fa0d5548a21727e32640a1ec09615
Artiom Tiurin <[artiom37@gmail.com](mailto:artiom37@gmail.com)>
2018-01-07 14:16:20 -0500

    Destini project completed.

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,11 +2,16 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
-
+    TextView mStoryView;
+    Button mTopButton, mBottomButton;
+    int mStoryIndex = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +20,118 @@ public class MainActivity extends AppCompatActivity {
 
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+        mStoryView = (TextView) findViewById(R.id.storyTextView);
 
+        mTopButton = (Button) findViewById(R.id.buttonTop);
+        mBottomButton = (Button) findViewById(R.id.buttonBottom);
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+        mTopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mStoryIndex++;
+                if(mStoryIndex == 2 && mStoryIndex > 0) {
+                    mStoryView.setText(R.string.T3_Story);
+                    mTopButton.setText(R.string.T3_Ans1);
+                    mBottomButton.setText(R.string.T3_Ans2);
 
+                    mTopButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            mStoryIndex++;
+                            if(mStoryIndex == 3) {
+                                mStoryView.setText(R.string.T6_End);
+                                //hide buttons
+                                mTopButton.setVisibility(View.GONE);
+                                mBottomButton.setVisibility(View.GONE);
+                            }
+                            mStoryIndex = 1;
+                        }
+                    });
+                    mBottomButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            mStoryIndex++;
+                            if(mStoryIndex == 3) {
+                                mStoryView.setText(R.string.T5_End);
+                                mTopButton.setVisibility(View.GONE);
+                                mBottomButton.setVisibility(View.GONE);
+                            }
+                            mStoryIndex = 1;
+                        }
+                    });
+                }
+            }
+        });
 
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+        mBottomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mStoryIndex++;
 
+                if(mStoryIndex == 2 && mStoryIndex > 0) {
+                    mStoryView.setText(R.string.T2_Story);
+                    mTopButton.setText(R.string.T2_Ans1);
+                    mBottomButton.setText(R.string.T2_Ans2);
+
+                    mBottomButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            mStoryIndex++;
+                            if(mStoryIndex == 3) {
+                                mStoryView.setText(R.string.T4_End);
+                                //hide buttons
+                                mTopButton.setVisibility(View.GONE);
+                                mBottomButton.setVisibility(View.GONE);
+                            }
+                            mStoryIndex = 1;
+                        }
+                    });
+
+                    mTopButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            mStoryIndex++;
+                            if(mStoryIndex == 3) {
+                                mStoryView.setText(R.string.T3_Story);
+                                mTopButton.setText(R.string.T3_Ans1);
+                                mBottomButton.setText(R.string.T3_Ans2);
+
+                                mTopButton.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        mStoryIndex++;
+                                        if(mStoryIndex == 4) {
+                                            mStoryView.setText(R.string.T6_End);
+                                            mTopButton.setVisibility(View.GONE);
+                                            mBottomButton.setVisibility(View.GONE);
+                                        }
+                                        mStoryIndex = 1;
+                                    }
+                                });
+                                mBottomButton.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        mStoryIndex++;
+                                        if(mStoryIndex == 4) {
+                                            mStoryView.setText(R.string.T5_End);
+                                            mTopButton.setVisibility(View.GONE);
+                                            mBottomButton.setVisibility(View.GONE);
+                                        }
+                                        mStoryIndex = 1;
+                                    }
+                                });
+                            }
+
+                        }
+                    });
+                }
+            }
+        });
 
     }
 }
